473bcf9ba7aaca2aae583ef505c801f3aba94cca
CloudBogdan <for_devs908@mail.ru>
2021-05-31 17:37:51 +0400

    Home page

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomePage = ()=> {
+    return (
+        <main className="page">
+
+            <div>Wow</div>
+            <div>Wow</div>
+            <div>Wow</div>
+
+        </main>
+    );
+};
+
+export default HomePage;
