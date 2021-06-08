c22bf6f77881212e5bb1aab80dd72da92a00fc2e
CtrlAltDelit <netrogaidengi@gmail.com>
2021-06-08 15:39:19 +0400

    Header update

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Icon from "../ui/Icon";
+import Logo from "../../imgs/logo_white-blue.svg"
 
 const Header = ()=> {
     return (
         <header className="header">
-
-            <b>Epam HR system</b>
+            
+            <div className="slot gap-2">
+                <img src={ Logo } alt="logo" />
+                <b style={{ whiteSpace: "nowrap" }}>HR system</b>
+            </div>
 
             <button title="Вы вошли как HR" className="slot-button subtle flex gap-2">
                 <Icon icon="bag" />
